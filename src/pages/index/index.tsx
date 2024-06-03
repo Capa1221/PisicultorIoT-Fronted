30940f8f5af39c1a9b167af82b487ac6b49ab4bc
Mankatico <anderssonbustos@gmail.com>
2024-06-03 01:09:49 -0500

    Añadiendo landing page

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,7 @@
 import { Applicationform } from '../../components/applicationForm/Applicationform'
 import { Characteristics } from '../../components/characteristics/Characteristics'
 import { Developers } from '../../components/developers/Developers'
+import { Footer } from '../../components/footer/Footer'
 import { Header } from '../../components/header/Header'
 import { Hero } from '../../components/hero/Hero'
 import { Technologies } from '../../components/technologies/Technologies'
@@ -14,7 +15,7 @@ export const Index  =() => {
       <Characteristics />
       <Developers />
       <Applicationform />
-      
+      <Footer />
     </div>
   )
 }
