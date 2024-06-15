40911d80f347ad087396fc288a1c5efad6f0ef69
Andersson <anderssonbustos@gmail.com>
2024-06-15 17:51:56 -0500

    Agregando campos faltantes

--- src/components/Hibernadero/modalAgregar.tsx
+++ src/components/Hibernadero/modalAgregar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Modal, Input, Textarea, ModalFooter, ModalContent, ModalBody, ModalHeader, Button, Image } from "@nextui-org/react";
+import { Modal, Input, Textarea, ModalFooter, ModalContent, ModalBody, ModalHeader, Button, Image, DropdownTrigger, Dropdown, DropdownMenu, DropdownItem, Select, SelectItem } from "@nextui-org/react";
 import { BiImage } from "react-icons/bi";
 
 interface NewHibernaderoModalProps {
@@ -38,21 +38,21 @@ const NewHibernaderoModal: React.FC<NewHibernaderoModalProps> = ({
             <ModalHeader className="flex flex-col gap-1">Agregar Nuevo Hibernadero</ModalHeader>
             <ModalBody>
               <div className="flex items-center space-x-2 m-0">
-              <Input
-                type="file"
-                name="image"
-                startContent={<BiImage className="text-2xl" />}
-                onChange={handleImageChange}
-              />
-              {imagePreview && (
-                <div className="flex place-content-end">
-                  <Image
-                    width={100}
-                    alt="Vista previa de la imagen"
-                    src={imagePreview}
-                  />
-                </div>
-              )}
+                <Input
+                  type="file"
+                  name="image"
+                  startContent={<BiImage className="text-2xl" />}
+                  onChange={handleImageChange}
+                />
+                {imagePreview && (
+                  <div className="flex place-content-end">
+                    <Image
+                      width={100}
+                      alt="Vista previa de la imagen"
+                      src={imagePreview}
+                    />
+                  </div>
+                )}
               </div>
               <Input
                 label="Nombre"
@@ -61,6 +61,23 @@ const NewHibernaderoModal: React.FC<NewHibernaderoModalProps> = ({
                 onChange={handleChangeNuevo}
                 width="100%"
               />
+              <Select
+                label="Cultivo"
+                placeholder="Seleccione el tipo de cultivo"
+              >
+                <SelectItem key={"0"} >
+                  Helllouuda
+                </SelectItem>
+                <SelectItem key={"1"} >
+                  Helllouudad
+                </SelectItem>
+                <SelectItem key={"2"} >
+                  Helllouudas
+                </SelectItem>
+                <SelectItem key={"3"} >
+                  Helllouudaa
+                </SelectItem>
+              </Select>
               <Input
                 label="Ciudad"
                 name="ciudad"
