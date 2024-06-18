ec3f3c709679e40cdf14867a82df053b48623ead
Andersson <anderssonmunoz.31@gmail.com>
2024-06-18 15:13:45 -0500

    Refactor components

---
+++ src/components/sensores/CardSensores.tsx
@@ -0,0 +1,38 @@
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider } from "@nextui-org/react";
+import { MdSensors } from "react-icons/md";
+import { BsCheckAll } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { CgData } from "react-icons/cg";
+import { ModalEliminarSensor } from "./ModalEliminarSensor";
+import { ModalEditarSensor } from "./ModalEditarSensor";
+
+export const CardSensores = () => {
+  return (
+    <>
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <MdSensors className="text-5xl text-warning" />
+          <div className="flex flex-col">
+            <p className="text-md font-semibold text-xl uppercase ">Sensor de Agua</p>
+            <div className="flex justify-around items-center space-x-5">
+              <p className="text-small text-default-500">Norte de Santander / Pamplona</p>
+              <Button className="w-min p-1 text-primary font-sans font-semibold" color="primary" variant="bordered" startContent={<BsCheckAll className="text-2xl" />}>Configurado</Button>
+            </div>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At aperiam asperiores, magni dicta atque, incidunt ea ipsa quaerat dignissimos amet dolore laboriosam ullam neque, exercitationem consequuntur delectus velit. Mollitia, eos.</p>
+        </CardBody>
+        <Divider />
+        <CardFooter className="justify-around">
+          <div className="space-x-2">
+            <ModalEliminarSensor />
+            <ModalEditarSensor />
+          </div>
+          <Link to={"/dashboard/Sensor/Grafica"}><Button color="primary" startContent={<CgData className="text-xl" />}>Ver datos</Button></Link>
+        </CardFooter>
+      </Card>
+    </>
+  )
+}
--- src/pages/dashboard/pages/Sensores.tsx
+++ src/pages/dashboard/pages/Sensores.tsx
@@ -1,13 +1,7 @@
 import { HeaderDashboard } from "../../../components/header/HeaderDashboard";
 import { CommentSection } from "../../../components/comment-dashboard/comment";
 import { ModalAgregarSensor } from "../../../components/sensores/ModalAgregarSensor";
-import { Button, Card, CardBody, CardFooter, CardHeader, Divider } from "@nextui-org/react";
-import { MdSensors } from "react-icons/md";
-import { BsCheckAll } from "react-icons/bs";
-import { ModalEliminarSensor } from "../../../components/sensores/ModalEliminarSensor";
-import { ModalEditarSensor } from "../../../components/sensores/ModalEditarSensor";
-import { Link } from "react-router-dom";
-import { CgData } from "react-icons/cg";
+import { CardSensores } from "../../../components/sensores/CardSensores";
 
 const Sensores = () => {
 
@@ -18,30 +12,7 @@ const Sensores = () => {
         <CommentSection mensaje="Aquí puedes gestionar los sensores asociados a los invernaderos. Puedes agregar nuevos sensores, buscar sensores especï¿½ficos y eliminar sensores existentes." />
         <ModalAgregarSensor />
         <div className="my-4">
-          <Card className="max-w-[400px]">
-            <CardHeader className="flex gap-3">
-              <MdSensors className="text-5xl text-warning" />
-              <div className="flex flex-col">
-                <p className="text-md font-semibold text-xl uppercase ">Sensor de Agua</p>
-                <div className="flex justify-around items-center space-x-5">
-                  <p className="text-small text-default-500">Norte de Santander / Pamplona</p>
-                  <Button className="w-min p-1 text-primary font-sans font-semibold" color="primary" variant="bordered" startContent={<BsCheckAll className="text-2xl" />}>Configurado</Button>
-                </div>
-              </div>
-            </CardHeader>
-            <Divider />
-            <CardBody>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At aperiam asperiores, magni dicta atque, incidunt ea ipsa quaerat dignissimos amet dolore laboriosam ullam neque, exercitationem consequuntur delectus velit. Mollitia, eos.</p>
-            </CardBody>
-            <Divider />
-            <CardFooter className="justify-around">
-              <div className="space-x-2">
-                <ModalEliminarSensor />
-                <ModalEditarSensor />
-              </div>
-                <Link to={"/dashboard/Sensor/Grafica"}><Button color="primary" startContent={<CgData className="text-xl"/>}>Ver datos</Button></Link>
-            </CardFooter>
-          </Card>
+          <CardSensores/>
         </div>
       </div>
     </>
