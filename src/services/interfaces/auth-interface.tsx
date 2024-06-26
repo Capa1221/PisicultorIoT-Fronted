036b861df9ccb3432f15ce4ff905f8aa58ffae8d
Andersson <anderssonmunoz.31@gmail.com>
2024-06-26 10:02:49 -0500

    Agregando interfaces de auth

---
+++ src/services/interfaces/auth-interface.tsx
@@ -0,0 +1,14 @@
+interface RegisterUser {
+  "usuario": string,
+  "nombres": string,
+  "email": string,
+  "clave": string
+}
+
+interface LoginUser {
+  "usuario": string,
+  "clave": string
+}
+
+
+export type {RegisterUser,LoginUser};
