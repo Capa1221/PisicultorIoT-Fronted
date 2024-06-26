2e74358fc37507142c2c5653142e5fd2b99e6d81
Andersson <anderssonmunoz.31@gmail.com>
2024-06-26 16:32:47 -0500

    Agregando table funcional users

--- src/components/users/TableUser.tsx
+++ src/components/users/TableUser.tsx
@@ -1,9 +1,29 @@
 import { Button, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip } from "@nextui-org/react";
 import { BiEdit } from "react-icons/bi";
 import { FaRegTrashAlt } from "react-icons/fa";
-import { InputPasswordUser } from "./InputPasswordUser";
+import { UserInterface } from "../../services/interfaces";
+import { useEffect, useState } from "react";
+import { buscarTodosLosUsuarios } from "../../services";
 
 export const TableUser = () => {
+  const [usersSistema, setUsersSistema] = useState<UserInterface[]>([]);
+  const token = sessionStorage.getItem("authToken");
+
+  useEffect(()=>{
+    const fetchApi = async ()=>{
+      try {
+        if(token){
+          const response = await buscarTodosLosUsuarios(token);
+          setUsersSistema(response.data);
+        }else{
+          console.error("error token");
+        }
+      } catch (error) {
+        console.error("fetch error users",error)
+      }
+    }
+    fetchApi();
+  },[token])
 
   return (
     <>
@@ -12,90 +32,28 @@ export const TableUser = () => {
           <TableColumn className="text-center uppercase">Usuario</TableColumn>
           <TableColumn className="text-center uppercase">Nombre</TableColumn>
           <TableColumn className="text-center uppercase">Email</TableColumn>
-          <TableColumn className="text-center uppercase w-48">Clave</TableColumn>
           <TableColumn className="text-center uppercase">Opciones</TableColumn>
         </TableHeader>
         <TableBody>
-          <TableRow key="4">
-            <TableCell className="text-center">William_Howard</TableCell>
-            <TableCell className="text-center">William Howard</TableCell>
-            <TableCell className="text-center">WilliamHoward@gmail.com</TableCell>
-            <TableCell className="text-center w-10">
-              <InputPasswordUser />
-            </TableCell>
-            <TableCell className="flex place-content-center">
-              <Tooltip content="Edit user">
-                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                  <Button color="warning" startContent={<BiEdit className="text-xl" />} variant="light"></Button>
-                </span>
-              </Tooltip>
-              <Tooltip color="danger" content="Delete user">
-                <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                  <Button color="danger" startContent={<FaRegTrashAlt className="text-xl" />} variant="light"></Button>
-                </span>
-              </Tooltip>
-            </TableCell>
-          </TableRow>
-          <TableRow key="5">
-            <TableCell className="text-center">William_Howard</TableCell>
-            <TableCell className="text-center">William Howard</TableCell>
-            <TableCell className="text-center">WilliamHoward@gmail.com</TableCell>
-            <TableCell className="text-center w-10">
-              <InputPasswordUser />
-            </TableCell>
-            <TableCell className="flex place-content-center">
-              <Tooltip content="Edit user">
-                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                  <Button color="warning" startContent={<BiEdit className="text-xl" />} variant="light"></Button>
-                </span>
-              </Tooltip>
-              <Tooltip color="danger" content="Delete user">
-                <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                  <Button color="danger" startContent={<FaRegTrashAlt className="text-xl" />} variant="light"></Button>
-                </span>
-              </Tooltip>
-            </TableCell>
-          </TableRow>
-          <TableRow key="6">
-            <TableCell className="text-center">William_Howard</TableCell>
-            <TableCell className="text-center">William Howard</TableCell>
-            <TableCell className="text-center">WilliamHoward@gmail.com</TableCell>
-            <TableCell className="text-center w-10">
-              <InputPasswordUser />
-            </TableCell>
-            <TableCell className="flex place-content-center">
-              <Tooltip content="Edit user">
-                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                  <Button color="warning" startContent={<BiEdit className="text-xl" />} variant="light"></Button>
-                </span>
-              </Tooltip>
-              <Tooltip color="danger" content="Delete user">
-                <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                  <Button color="danger" startContent={<FaRegTrashAlt className="text-xl" />} variant="light"></Button>
-                </span>
-              </Tooltip>
-            </TableCell>
-          </TableRow>
-          <TableRow key="7">
-            <TableCell className="text-center">William_Howard</TableCell>
-            <TableCell className="text-center">William Howard</TableCell>
-            <TableCell className="text-center">WilliamHoward@gmail.com</TableCell>
-            <TableCell className="text-center w-10">
-              <InputPasswordUser />
-            </TableCell>
-            <TableCell className="flex place-content-center">
-              <Tooltip content="Edit user">
-                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                  <Button color="warning" startContent={<BiEdit className="text-xl" />} variant="light"></Button>
-                </span>
-              </Tooltip>
-              <Tooltip color="danger" content="Delete user">
-                <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                  <Button color="danger" startContent={<FaRegTrashAlt className="text-xl" />} variant="light"></Button>
-                </span>
-              </Tooltip>
-            </TableCell>
-          </TableRow>
+        {usersSistema.map((usersSistema) => (
+          <TableRow key={usersSistema.id}>
+          <TableCell className="text-center">{usersSistema.usuario}</TableCell>
+          <TableCell className="text-center">{usersSistema.nombres}</TableCell>
+          <TableCell className="text-center">{usersSistema.email}</TableCell>
+          <TableCell className="flex place-content-center">
+            <Tooltip content="Edit user">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <Button color="warning" startContent={<BiEdit className="text-xl" />} variant="light"></Button>
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                <Button color="danger" startContent={<FaRegTrashAlt className="text-xl" />} variant="light"></Button>
+              </span>
+            </Tooltip>
+          </TableCell>
+        </TableRow>
+        ))}
         </TableBody>
       </Table>
     </>
--- src/pages/dashboard/pages/Invernaderos.tsx
+++ src/pages/dashboard/pages/Invernaderos.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { CardHibernadero, CommentSection, HeaderDashboard, ModalAgregar } from "../../../components";
 import { decodeToken } from "../../../utils/utilsToken";
 
