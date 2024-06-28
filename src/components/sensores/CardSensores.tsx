3ba9f3adbb1d4aa345963c73d2bc4b3c67c4d5d8
Andersson <anderssonmunoz.31@gmail.com>
2024-06-28 15:29:40 -0500

    Agregando componentes de sensores

--- src/components/sensores/CardSensores.tsx
+++ src/components/sensores/CardSensores.tsx
@@ -5,24 +5,27 @@ import { Link } from "react-router-dom";
 import { CgData } from "react-icons/cg";
 import { ModalEliminarSensor } from "./ModalEliminarSensor";
 import { ModalEditarSensor } from "./ModalEditarSensor";
+import { SensorInterface } from "../../services/interfaces";
 
-export const CardSensores = () => {
+export const CardSensores = (sensor: SensorInterface) => {
   return (
     <>
       <Card className="max-w-[400px]">
         <CardHeader className="flex gap-3">
           <MdSensors className="text-5xl text-warning" />
           <div className="flex flex-col">
-            <p className="text-md font-semibold text-xl uppercase ">Sensor de Agua</p>
+            <p className="text-md font-semibold text-xl uppercase ">{sensor.nombre}</p>
             <div className="flex justify-around items-center space-x-5">
-              <p className="text-small text-default-500">Norte de Santander / Pamplona</p>
-              <Button className="w-min p-1 text-primary font-sans font-semibold" color="primary" variant="bordered" startContent={<BsCheckAll className="text-2xl" />}>Configurado</Button>
+              <p className="text-small text-default-500">{sensor.ubicacion}</p>
+              <Button className="w-min p-1 text-primary font-sans font-semibold" color="primary" variant="bordered" startContent={<BsCheckAll className="text-2xl" />}>{sensor.config}</Button>
             </div>
           </div>
         </CardHeader>
         <Divider />
         <CardBody>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At aperiam asperiores, magni dicta atque, incidunt ea ipsa quaerat dignissimos amet dolore laboriosam ullam neque, exercitationem consequuntur delectus velit. Mollitia, eos.</p>
+          <p>
+            {sensor.descripcion}
+          </p>
         </CardBody>
         <Divider />
         <CardFooter className="justify-around">
--- src/pages/dashboard/pages/Sensores.tsx
+++ src/pages/dashboard/pages/Sensores.tsx
@@ -4,25 +4,33 @@ import { ModalAgregarSensor } from "../../../components/sensores/ModalAgregarSen
 import { CardSensores } from "../../../components/sensores/CardSensores";
 import { useEffect, useState } from "react";
 import { SensorInterface } from "../../../services/interfaces";
+import { obtenerSensoresPorUsuario } from "../../../services/sensor-controller";
+import { decodeToken } from "../../../utils/utilsToken";
 
 export const Sensores = () => {
-  const token = sessionStorage.getItem("authToken");
+  const token = sessionStorage.getItem("authToken")!;
+  const decodetoken = decodeToken(token);
   const [sensores, setSensores] = useState<SensorInterface[]>([]);
 
-  useEffect(()=>{
-    const fetchSensores = async () =>{
+  useEffect(() => {
+    const fetchSensores = async () => {
       try {
-        if(token){
-          const response = await 
-        }else{
+        if (token) {
+          const response = await obtenerSensoresPorUsuario(decodetoken.idUsuario, token);
+          if (response.status == 200) {
+            setSensores(response.data);
+          } else {
+            console.error("error fetchSensores");
+          }
+        } else {
           console.error("error token");
         }
       } catch (error) {
-        
+        console.error("error", error);
       }
-
     }
-  },[token]);
+    fetchSensores();
+  }, [token]);
 
   return (
     <>
@@ -31,7 +39,15 @@ export const Sensores = () => {
         <CommentSection mensaje="Aquí puedes gestionar los sensores asociados a los invernaderos. Puedes agregar nuevos sensores, buscar sensores especï¿½ficos y eliminar sensores existentes." />
         <ModalAgregarSensor />
         <div className="my-4">
-          <CardSensores/>
+          {sensores.map((sensor) => (
+            <CardSensores
+              key={sensor.id}
+              id={sensor.id}
+              idHibernadero={sensor.idHibernadero}
+              nombre={sensor.nombre}
+              descripcion={sensor.descripcion}
+              config={sensor.config} />
+          ))}
         </div>
       </div>
     </>
