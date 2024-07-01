8444404dfe5b8b13e9ba7abee10650825e6bb2fc
Andersson <anderssonbustos@gmail.com>
2024-07-01 17:04:23 -0500

    Refactorizando las rutas por bug de no renderizar componentes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Spinnerlazyload from './components/spinner/spinner-lazy-load';
 
 function App() {
 
-  const Index = lazy(() => import('./pages/index'));
+  const Index = lazy(() => import('./pages/index/index'));
   const Login = lazy(() => import('./pages/auth/Login'));
   const Dashboard = lazy(() => import('./pages/dashboard/dashboard'));
   const ForgotPassword = lazy(() => import('./pages/auth/Forgot-Password'));
@@ -19,9 +19,9 @@ function App() {
           <Routes>
             <Route path='/' element={<Index />} />
             <Route path='/Iniciar-Sesion' element={<Login />} />
-            <Route path='/dashboard/*' element={<Dashboard />} />
             <Route path='/Olvidar-Clave' element={<ForgotPassword />} />
             <Route path="/Validar-Codigo/:usuario" element={<ValidarCodigo />} />
+            <Route path='/Dashboard/*' element={<Dashboard />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -2,11 +2,10 @@ import { useEffect, useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { buscarTodosLosUsuarios } from "../../services";
-import { Home,PageGraficasSensores,Sensores,Propiedades,Usuarios,EstacionesUser,EstacionesSistemaComponent,EstacionesAsociacion,ForgotPassword,} from '../index'
+import { Home, PageGraficasSensores, Sensores, Propiedades, Usuarios, EstacionesUser, EstacionesSistemaComponent, EstacionesAsociacion } from '../index'
 import adminRoutes from "../../routes/AdminRoutes.json";
 import userRoutes from "../../routes/UserRoutes.json";
 import { decodeToken } from "../../utils/utilsToken";
-import React from "react";
 
 // Definir tipos para las rutas
 type RouteItem = {
@@ -19,7 +18,7 @@ type RouteItem = {
 export const Dashboard = () => {
   const token = sessionStorage.getItem("authToken") || "";
   const navigate = useNavigate();
-  const [userRoutesList, setUserRoutesList] = useState<RouteItem[]>([]); // Especificar el tipo de datos aquí
+  const [userRoutesList, setUserRoutesList] = useState<RouteItem[]>([]); // Especificar el tipo de datos aquï¿½
 
   useEffect(() => {
     const fetchValidarToken = async () => {
@@ -30,9 +29,9 @@ export const Dashboard = () => {
           sessionStorage.removeItem("authToken");
           alert("Su Token ha expirado");
         } else {
-          // Determinar qué rutas cargar según el rol del usuario
+          // Determinar quï¿½ rutas cargar segï¿½n el rol del usuario
           const decodificarToken = decodeToken(token);
-          const isAdmin = decodificarToken.sub=="ROOT";
+          const isAdmin = decodificarToken.sub == "ROOT";
 
           if (isAdmin) {
             setUserRoutesList(adminRoutes as RouteItem[]); // Castear a RouteItem[]
@@ -52,18 +51,16 @@ export const Dashboard = () => {
   return (
     <div className="grid lg:grid-cols-4 xl:grid-cols-6 min-h-screen">
       <Sidebar routes={userRoutesList} /> {/* Pasar las rutas al componente Sidebar */}
-      <main className="lg:col-span-3 xl:col-span-5 bg-gray-100 p-8 h-[100vh] overflow-y-scroll">
+      <main className="lg:col-span-3 xl:col-span-5 bg-gray-100 p0-8 h-[100vh] overflow-y-scroll">
         <Routes>
-          {/* Iterar sobre las rutas dinámicamente */}
-          {userRoutesList.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={React.createElement(eval(route.component))}
-            />
-          ))}
-          {/* Ruta para el Home */}
-          <Route path="/" element={<Home />} />
+          <Route index path="/Home" element={<Home />} />
+          <Route path="/Mis-Estaciones" element={<EstacionesUser />} />
+          <Route path="/Asociacion-Estaciones" element={<EstacionesAsociacion />} />
+          <Route path="/Usuarios" element={<Usuarios />} />
+          <Route path="/Sensores" element={<Sensores />} />
+          <Route path="/Estaciones-Sistema" element={<EstacionesSistemaComponent />} />
+          <Route path="/propiedades" element={<Propiedades />} />
+          <Route path="/Graficas-Sensor/:id" element={<PageGraficasSensores />} />
         </Routes>
       </main>
     </div>
